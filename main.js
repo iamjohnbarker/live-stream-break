fd7533014eee13916218e0640e6386a050067e24
John Barker <iamjohnbarker@hotmail.com>
2018-06-16 20:49:16 +0100

    Loading order update

--- main.js
+++ main.js
@@ -88,9 +88,9 @@ function createWindowThree () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
-app.on('ready', createWindowTwo)
 app.on('ready', createWindowThree)
+app.on('ready', createWindowTwo)
+app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
