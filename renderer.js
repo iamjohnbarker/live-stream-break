d29e1caa0c0f6372acae7ac56d0054a45598b393
John Barker <iamjohnbarker@hotmail.com>
2020-01-19 09:10:49 +0100

    change laptop

--- renderer.js
+++ renderer.js
@@ -1,89 +1,91 @@
-const electron = require('electron');
-const {ipcRenderer} = require('electron');
-const {ipcMain} = require('electron');
-var gsjson = require('google-spreadsheet-to-json');
+const electron = require("electron");
+const { ipcRenderer } = require("electron");
+const { ipcMain } = require("electron");
+var gsjson = require("google-spreadsheet-to-json");
 
-var dropdown = document.getElementById('talkPicker');
-var spreadsheetID = document.getElementById('spreadsheetID').addEventListener('change', function (e) {
+var dropdown = document.getElementById("talkPicker");
+var spreadsheetID = document
+	.getElementById("spreadsheetID")
+	.addEventListener("change", function(e) {
+		optionDefault = document.createElement("option");
+		optionDefault.text = "Grabbing data, hold tight...";
+		dropdown.remove(dropdown[0]);
+		dropdown.add(optionDefault, 0);
 
-optionDefault = document.createElement('option');
-optionDefault.text = "Grabbing data, hold tight...";
-dropdown.remove(dropdown[0]);
-dropdown.add(optionDefault, 0);
+		gsjson({
+			spreadsheetId: e.srcElement.value
+			// other options...
+		})
+			.then(function(result) {
+				console.log(e.srcElement.value);
+				console.log(result.length);
+				console.log(result);
+				optionDefault = document.createElement("option");
+				optionDefault.text = "Choose a talk...";
+				dropdown.remove(dropdown[0]);
+				dropdown.add(optionDefault, 0);
+				for (var i = 0; i < result.length; i++) {
+					option = document.createElement("option");
+					option.text = result[i].speakerName + " - " + result[i].talkTitle;
+					option.value = i;
+					option.talkTitle = result[i].talkTitle;
+					option.speakerName = result[i].speakerName;
+					option.talkTime = result[i].talkTime;
+					option.talkDate = result[i].talkDate;
+					dropdown.add(option);
+				}
+			})
+			.catch(function(err) {
+				console.log(err.message);
+				console.log(err.stack);
+			});
 
-gsjson({
-    spreadsheetId: e.srcElement.value,
-    // other options...
-})
-.then(function(result) {
-  console.log(e.srcElement.value);
-  console.log(result.length);
-  console.log(result);
-  optionDefault = document.createElement('option');
-  optionDefault.text = "Choose a talk...";
-  dropdown.remove(dropdown[0]);
-  dropdown.add(optionDefault, 0);
-  for (var i = 0; i < result.length; i++) {
-    option = document.createElement('option');
-    option.text = result[i].speakerName+' - '+result[i].talkTitle;
-    option.value = i;
-    option.talkTitle = result[i].talkTitle;
-    option.speakerName = result[i].speakerName;
-    option.talkTime = result[i].talkTime;
-    option.talkDate = result[i].talkDate;
-    dropdown.add(option);
-  }
-})
-.catch(function(err) {
-  console.log(err.message);
-  console.log(err.stack);
-});
+		document
+			.getElementById("talkPicker")
+			.addEventListener("change", function(e) {
+				console.log(e);
+				document.getElementById("speakerName").value =
+					e.target.selectedOptions[0].talkTitle;
+				document.getElementById("speakerTitle").value =
+					e.target.selectedOptions[0].speakerName;
 
-document.getElementById('talkPicker').addEventListener('change', function(e) {
-  console.log(e);
-  document.getElementById('speakerName').value = e.target.selectedOptions[0].talkTitle;
-  document.getElementById('speakerTitle').value = e.target.selectedOptions[0].speakerName;
-
-  function minTommss(minutes){
-   var hrs = Math.floor(Math.abs(minutes) * 24);
-   var min = ((minutes * 24) - hrs);
-   var min2 = Math.floor(Math.abs(min) * 60.5);
-   console.log(min);
-   return hrs + ":" + (min2 < 10 ? "0" : "") + min2;
-  }
-  document.getElementById('trackTime').value = minTommss(e.target.selectedOptions[0].talkTime);
-})
-
-})
+				function minTommss(minutes) {
+					var hrs = Math.floor(Math.abs(minutes) * 24);
+					var min = minutes * 24 - hrs;
+					var min2 = Math.floor(Math.abs(min) * 60.5);
+					console.log(min);
+					return hrs + ":" + (min2 < 10 ? "0" : "") + min2;
+				}
+				document.getElementById("trackTime").value = minTommss(
+					e.target.selectedOptions[0].talkTime
+				);
+			});
+	});
 
 //SEND SPEAKER INFO
-document.getElementById('sendToMain').addEventListener('click', buttonClicked);
+document.getElementById("sendToMain").addEventListener("click", buttonClicked);
 function buttonClicked(e) {
-  e.preventDefault();
-  var item = document.getElementById('speakerName').value;
-  var item2 = document.getElementById('speakerTitle').value;
-  var item3 = document.getElementById('trackTime').value;
-  var item4 = document.getElementById('trackZone').value;
-  var item5 = document.getElementById('speakerImage').value;
-  var item6 = document.getElementById('eventLogo').value;
-  var item7 = document.getElementById('eventName').value;
-  ipcRenderer.send('button', item, item2, item3, item4, item5, item6, item7);
+	e.preventDefault();
+	var item = document.getElementById("speakerName").value;
+	var item2 = document.getElementById("speakerTitle").value;
+	var item3 = document.getElementById("trackTime").value;
+	var item4 = document.getElementById("trackZone").value;
+	var item5 = document.getElementById("speakerImage").value;
+	var item6 = document.getElementById("eventLogo").value;
+	var item7 = document.getElementById("eventName").value;
+	ipcRenderer.send("button", item, item2, item3, item4, item5, item6, item7);
 }
-
 
 // SCREENSHOT LOGIC
 
-const {desktopCapturer, screen, shell} = require('electron')
-const fs = require('fs')
-const os = require('os')
-const path = require('path')
+const { desktopCapturer, screen, shell } = require("electron");
+const fs = require("fs");
+const os = require("os");
+const path = require("path");
 //var moment = require('moment');
 //var timr = require('timr');
 //var scheduler = timr();
 //var looksSame = require('looks-same');
-
-
-
 
 // desktopCapturer.getSources({types: ['window', 'screen']}, (error, sources) => {
 //   if (error) throw error
@@ -123,79 +125,154 @@ const path = require('path')
 //
 // var d = Date();
 
-const screenshot = document.getElementById('screen-shot')
-const screenshotMsg = document.getElementById('screenshot-path')
+const screenshot = document.getElementById("screen-shot");
+const screenshotMsg = document.getElementById("screenshot-path");
 
-screenshot.addEventListener('click', (event) => {
-    screenshotMsg.textContent = 'Gathering screens...'
-    const thumbSize = determineScreenShotSize()
-    let options = { types: ['window'], thumbnailSize: thumbSize }
+screenshot.addEventListener("click", event => {
+	screenshotMsg.textContent = "Gathering screens...";
+	const thumbSize = determineScreenShotSize();
+	let options = { types: ["window"], thumbnailSize: thumbSize };
 
-    desktopCapturer.getSources(options, (error, sources) => {
-      if (error) return console.log(error)
+	desktopCapturer.getSources(options, (error, sources) => {
+		if (error) return console.log(error);
 
-      sources.forEach((source) => {
-        if (source.name === 'Movie Recording') {
-          const screenshotPath = path.join(os.homedir(), '/screenshots/captured.png')
+		sources.forEach(source => {
+			if (source.name === "Movie Recording") {
+				const screenshotPath = path.join(
+					os.homedir(),
+					"/screenshots/captured.png"
+				);
 
-          fs.writeFile(screenshotPath, source.thumbnail.toPNG(), (error) => {
-            if (error) return console.log(error)
-            //shell.openExternal(`file://${screenshotPath}`)
+				fs.writeFile(screenshotPath, source.thumbnail.toPNG(), error => {
+					if (error) return console.log(error);
+					//shell.openExternal(`file://${screenshotPath}`)
 
-            const message = `Saved screenshot to: ${screenshotPath}`
-            screenshotMsg.textContent = message
-          })
-        }
-      })
-    })
+					const message = `Saved screenshot to: ${screenshotPath}`;
+					screenshotMsg.textContent = message;
+				});
+			}
+		});
+	});
+});
 
-
-})
-
-function determineScreenShotSize () {
-  const screenSize = screen.getPrimaryDisplay().workAreaSize
-  const maxDimension = Math.max(screenSize.width, screenSize.height)
-  return {
-    width: 1920,
-    height: 1080
-  }
+function determineScreenShotSize() {
+	const screenSize = screen.getPrimaryDisplay().workAreaSize;
+	const maxDimension = Math.max(screenSize.width, screenSize.height);
+	return {
+		width: 1920,
+		height: 1080
+	};
 }
 
+const screenshotShare = document.getElementById("screen-shot-share");
 
-const screenshotShare = document.getElementById('screen-shot-share')
+screenshotShare.addEventListener("click", event => {
+	screenshotMsg.textContent = "Gathering screens...";
+	const thumbSize = determineScreenShotSize();
+	let options = { types: ["window"], thumbnailSize: thumbSize };
 
-screenshotShare.addEventListener('click', (event) => {
-    screenshotMsg.textContent = 'Gathering screens...'
-    const thumbSize = determineScreenShotSize()
-    let options = { types: ['window'], thumbnailSize: thumbSize }
+	desktopCapturer.getSources(options, (error, sources) => {
+		if (error) return console.log(error);
 
-    desktopCapturer.getSources(options, (error, sources) => {
-      if (error) return console.log(error)
+		sources.forEach(source => {
+			console.log(source.name);
+			if (source.name === "displayScreenshot") {
+				const screenshotPath = path.join(
+					os.homedir(),
+					"/screenshots/share.png"
+				);
 
-      sources.forEach((source) => {
-        console.log(source.name);
-        if (source.name === 'displayScreenshot') {
-          const screenshotPath = path.join(os.homedir(), '/screenshots/share.png')
+				fs.writeFile(screenshotPath, source.thumbnail.toPNG(), error => {
+					if (error) return console.log(error);
+					//shell.openExternal(`file://${screenshotPath}`)
 
-          fs.writeFile(screenshotPath, source.thumbnail.toPNG(), (error) => {
-            if (error) return console.log(error)
-            //shell.openExternal(`file://${screenshotPath}`)
+					const message = `Saved screenshot to: ${screenshotPath}`;
+					screenshotMsg.textContent = message;
+				});
+			}
+		});
+	});
+});
 
-            const message = `Saved screenshot to: ${screenshotPath}`
-            screenshotMsg.textContent = message
-          })
-        }
-      })
-    })
+function determineScreenShotSize() {
+	const screenSize = screen.getPrimaryDisplay().workAreaSize;
+	const maxDimension = Math.max(screenSize.width, screenSize.height);
+	return {
+		width: 1920,
+		height: 1080
+	};
+}
 
+const constraints = {
+	audio: false,
+	video: {
+		mandatory: {
+			maxHeight: 480,
+			maxWidth: 800,
+			minHeight: 480,
+			minWidth: 800
+		}
+	}
+};
 
-})
+const videoElement = document.querySelector("video");
+const audioSelect = document.querySelector("select#audioSource");
+const videoSelect = document.querySelector("select#videoSource");
 
-function determineScreenShotSize () {
-  const screenSize = screen.getPrimaryDisplay().workAreaSize
-  const maxDimension = Math.max(screenSize.width, screenSize.height)
-  return {
-    width: 1920,
-    height: 1080
-  }
+navigator.mediaDevices
+	.enumerateDevices()
+	.then(gotDevices)
+	.then(getStream)
+	.catch(handleError);
+
+audioSelect.onchange = getStream;
+videoSelect.onchange = getStream;
+
+function gotDevices(deviceInfos) {
+	for (let i = 0; i !== deviceInfos.length; ++i) {
+		const deviceInfo = deviceInfos[i];
+		const option = document.createElement("option");
+		option.value = deviceInfo.deviceId;
+		if (deviceInfo.kind === "audioinput") {
+			option.text =
+				deviceInfo.label || "microphone " + (audioSelect.length + 1);
+			audioSelect.appendChild(option);
+		} else if (deviceInfo.kind === "videoinput") {
+			option.text = deviceInfo.label || "camera " + (videoSelect.length + 1);
+			videoSelect.appendChild(option);
+		} else {
+			console.log("Found another kind of device: ", deviceInfo);
+		}
+	}
+}
+
+function getStream() {
+	if (window.stream) {
+		window.stream.getTracks().forEach(function(track) {
+			track.stop();
+		});
+	}
+
+	const constraints = {
+		audio: {
+			deviceId: { exact: audioSelect.value }
+		},
+		video: {
+			deviceId: { exact: videoSelect.value }
+		}
+	};
+
+	navigator.mediaDevices
+		.getUserMedia(constraints)
+		.then(gotStream)
+		.catch(handleError);
+}
+
+function gotStream(stream) {
+	window.stream = stream; // make stream available to console
+	videoElement.srcObject = stream;
+}
+
+function handleError(error) {
+	console.error("Error: ", error);
 }
